15a16fffddc28f4065ee2a3adce06f1b6aa3d9e7
BrunoCorreia-hub <correiabruno346@gmail.com>
2025-04-07 22:42:13 -0300

    alteracao no config do vit adicionado base

--- devburger-interface/vite.config.js
+++ devburger-interface/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projeto-fullstack-devburger/', // 👈 Isso é ESSENCIAL!
   plugins: [react()],
 })
